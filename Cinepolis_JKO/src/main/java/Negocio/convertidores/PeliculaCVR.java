93c4b07bbcb3636f4f89ba02accab6e1c294e6bd
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2024-06-11 12:49:48 -0700

    Update PeliculaCVR.java
    se crearon los convertidores para pelicula

--- Cinepolis_JKO/src/main/java/Negocio/convertidores/PeliculaCVR.java
+++ Cinepolis_JKO/src/main/java/Negocio/convertidores/PeliculaCVR.java
@@ -4,10 +4,39 @@
  */
 package Negocio.convertidores;
 
+import DTO.PeliculaDTO;
+import entidad.Pelicula;
+
 /**
  *
  * @author skevi
  */
 public class PeliculaCVR {
+    
+    public Pelicula peliculaDTO_entidad(Pelicula pelicula){
+        int idPelicula = pelicula.getIdPelicula();
+        String titulo = pelicula.getTitulo();
+        String sinopsis = pelicula.getSinopsis();
+        int idPais = pelicula.getIdPais();
+        int duracion = pelicula.getDuracion();
+        String clasificacion = pelicula.getClasificacion();
+        String trailer = pelicula.getTrailer();
+        String imagen = pelicula.getImagen();
+        int idGenero = pelicula.getIdGenero();
+        return new Pelicula (idPelicula, titulo, sinopsis, idPais, duracion, clasificacion, trailer, imagen, idGenero);
+    } 
+    
+    public PeliculaDTO pelicula_DTO(PeliculaDTO peliculaDTO){
+        int idPelicula = peliculaDTO.getIdPelicula();
+        String titulo = peliculaDTO.getTitulo();
+        String sinopsis = peliculaDTO.getSinopsis();
+        int idPais = peliculaDTO.getIdPais();
+        int duracion = peliculaDTO.getDuracion();
+        String clasificacion = peliculaDTO.getClasificacion();
+        String trailer = peliculaDTO.getTrailer();
+        String imagen = peliculaDTO.getImagen();
+        int idGenero = peliculaDTO.getIdGenero();
+        return new PeliculaDTO (idPelicula, titulo, sinopsis, idPais, duracion, clasificacion, trailer, imagen, idGenero);
+    }
     
 }
